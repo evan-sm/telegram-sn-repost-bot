96fa87163b1672855d96066ed060af9ee7832d30
wMw <wmwofficial@gmail.com>
2018-05-20 20:51:10 +0300

    0

--- tele.py
+++ tele.py
@@ -50,9 +50,9 @@ def teleSendPhotoSS(who, img, desc):
 		TKN = TKN_TELE_IWB
 	URL_TELE_API = 'https://api.telegram.org/bot%s/' % TKN # Telegram API URL
 	#remote_file = requests.get(url)
-	file = img.seek(0)
+	#file = img.seek(0)
 	data = {'chat_id' : chan, 'caption': desc, 'parse_mode': 'Markdown'}
-	files = {'photo': file}
+	files = {'photo': img}
 	r = requests.post(URL_TELE_API + 'sendPhoto', files=files, data=data)	
 	print (r.text)	
 	return r
